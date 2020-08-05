0adc55fc3277b2dddd532ec5e68194f6a12193c6
Prasan Dhareshwar <prasandhareshwar@gmai.com>
2020-08-05 04:04:19 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,242 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>My Music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">
+          {{ current.title }} -
+          <span>{{current.movie}}</span>
+        </h2>
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>Playlist</h3>
+        <button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="(song.src == current.src) ? 'song playing' : 'song'"
+        >{{ song.title }} - {{ song.movie }}</button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      //List of songs
+      songs: [
+        {
+          title: "Afreeda",
+          movie: "Dil Bechara",
+          src: require(""), //Song Source
+        },
+        {
+          title: "Aur Tanha",
+          movie: "Love Aaj Kal",
+          src: require(""),
+        },
+        {
+          title: "Dhak Dhak",
+          movie: "Love Aaj Kal",
+          src: require(""),
+        },
+        {
+          title: "Dil Bechara",
+          movie: "Dil Bechara",
+          src: require(""),
+        },
+        {
+          title: "Ek Zindagi",
+          movie: "Angrezi Medium",
+          src: require("./assets/Ek Zindagi - Angrezi Medium.mp3"),
+        },
+      ],
+      player: new Audio(),
+    };
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.player.addEventListener(
+        "ended",
+        function () {
+          if (this.index > this.songs.length - 1) {
+            this.index = 0;
+          }
+
+          this.current = this.songs[this.index];
+
+          this.play(this.current);
+        }.bind(this)
+      );
+      this.isPlaying = true;
+    },
+
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+
+    next() {
+      this.index++;
+
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+    },
+
+    prev() {
+      this.index--;
+
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+    },
+  },
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body {
+  font-family: sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: white;
+}
+
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+
+.song-title {
+  color: #212121;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: none;
+}
+
+.play, .pause{
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: white;
+  cursor: pointer;
+  background-color: tomato;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: white;
+  cursor: pointer;
+  background-color: rgb(126, 44, 29);
+}
+
+.playlist {
+  padding: 0px 30px;
+}
+
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover {
+  color: #FF5858;
+}
+
+.playlist .song.playing {
+  color: white;
+  background-image: linear-gradient(to right, tomato, rgb(126, 44, 29));
+}
+
 </style>
